8ccdda0ea33464674eb2b0ad5db8c188ed77a2a8
Jacin Montava <jmontava@gmail.com>
2016-04-28 17:52:19 +0200

    Tarea semana 2 terminada

--- JuegoDeMemoria.playground/Contents.swift
+++ JuegoDeMemoria.playground/Contents.swift
@@ -2,4 +2,42 @@
 
 import UIKit
 
+let range = 0...100
+
+
+for num in range {
+    
+    let range2 = 30...40
+    var string = "# \(num)"
+
+    if(num % 5 == 0) {
+        string += " -- Bingo!!!"
+    }
+    
+    if(num % 2 == 0) {
+        string += " -- par!!!"
+    }
+    
+    if(num % 2 != 0) {
+        string += " -- impar!!!"
+    }
+    
+    if(num > 29 && num < 41) {
+        string += " -- Viva Swift!!!"
+    }
+    
+    print (string)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
